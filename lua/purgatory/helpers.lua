bad99d203e6fa04655af35324bcdc497e52b94c3
ratludu <brandon.skewes@outlook.com>
2026-02-21 19:55:24 +1100

    added git hash for title instead of repo name

--- lua/purgatory/helpers.lua
+++ lua/purgatory/helpers.lua
@@ -1,23 +1,32 @@
 local M = {}
 
----@return nil|string
+--- returns the git repo friendly name
+--- and the git tree hash of the repo so that
+--- there should not be any collisions.
+---@return nil|string, nil|string
 function M.get_git_repo_name()
   local result = vim.fn.system("git rev-parse --show-toplevel 2>/dev/null")
   if vim.v.shell_error ~= 0 then
-    return nil -- not a git repo
+    return nil
   end
-  return vim.fn.fnamemodify(result:gsub("\n", ""), ":t")
+
+  local git_tree = vim.fn.system("git rev-list --max-parents=0 HEAD 2>/dev/null")
+  if vim.v.shell_error ~= 0 then
+    return nil
+  end
+
+  return vim.fn.fnamemodify(result:gsub("\n", ""), ":t"), vim.fn.fnamemodify(git_tree:gsub("\n", ""), ":t")
 end
 
+--- returns the filepath of the repo
 ---@return string
 function M.get_filepath()
-  local repo = M.get_git_repo_name()
-  if repo then
-    filepath = vim.fn.stdpath("data") .. "/purgatory/" .. repo .. ".txt"
+  local _, git_tree = M.get_git_repo_name()
+  if git_tree then
+    filepath = vim.fn.stdpath("data") .. "/purgatory/" .. git_tree .. ".txt"
   else
     filepath = vim.fn.stdpath("data") .. "/purgatory/default.txt"
   end
-
   vim.fn.mkdir(vim.fn.fnamemodify(filepath, ":h"), "p")
   vim.fn.expand(filepath)
   return filepath
@@ -25,6 +34,10 @@ end
 
 ---@param filepath string
 function M.check_buffer_exists(filepath)
+  if filepath ~= nil then
+    return
+  end
+
   local existing_buf = vim.fn.bufnr(filepath)
   if existing_buf ~= -1 then
     local existing_win = vim.fn.bufwinid(existing_buf)
@@ -48,21 +61,21 @@ function M.open_floating_window(opts)
   local col = math.floor((width - win_width) / 2)
 
   local buf = vim.api.nvim_create_buf(false, true)
-  local title_extension = M.get_git_repo_name()
+  local title_extension, _ = M.get_git_repo_name()
 
-  local title = "purgatory - no repo"
+  local title = " purgatory - no repo "
   if title_extension ~= nil then
-    title = "purgatory - " .. title_extension
+    title = " purgatory - " .. title_extension .. " "
   end
 
   local win_opts = {
-    relative = "editor", -- relative to the whole editor
+    relative = "editor",
     width = win_width,
     height = win_height,
     row = row,
     col = col,
-    style = "minimal",  -- no line numbers, sign column, etc.
-    border = "rounded", -- none | single | double | rounded | solid | shadow
+    style = "minimal",
+    border = "rounded",
     title = title,
     title_pos = "center",
   }
